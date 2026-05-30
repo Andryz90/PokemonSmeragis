0865f584db93fcd5a8ffe8c751ad2a46f5ab132a
Andrea Grieco <andre.vovinam@hotmail.it>
2026-05-30 12:46:05 +0200

    Modified Area Zero map + added header

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -571,6 +571,8 @@
 #define MAP_DEWFORD_MEADOW_HOUSE1F                          (91 | (26 << 8))
 #define MAP_AREA_ZERO_OUTSIDE                               (92 | (26 << 8))
 #define MAP_SLATEPORT_SEWER                                 (93 | (26 << 8))
+#define MAP_AREA_ZERO_INSIDE                                (94 | (26 << 8))
+#define MAP_AREA_ZERO_INSIDE_2                              (95 | (26 << 8))
 
 // gMapGroup_IndoorRoute104Prototype
 #define MAP_ROUTE104_PROTOTYPE                          (0 | (27 << 8))
